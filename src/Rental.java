25fbbabd89443d0168f35427f7ad9f2c2dbb21fa
Vinicius Rodrigues <viniciusr02@yahoo.com.br>
2022-06-16 17:49:52 -0300

    Commit 6

--- src/Rental.java
+++ src/Rental.java
@@ -38,5 +38,18 @@ public class Rental {
         return thisAmount;
     }
 
+    public int getFrequentRenterPoints() { 
+        int frequentRenterPoints = 0;
+
+        // add frequent renter points
+        frequentRenterPoints ++;
+        
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) 
+            frequentRenterPoints ++;
+
+        return frequentRenterPoints;
+    }  
+
 
 }
