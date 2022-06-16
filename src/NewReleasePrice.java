3493018ae1062d1daa45000215908be1165418a4
Vinicius Rodrigues <viniciusr02@yahoo.com.br>
2022-06-16 19:10:02 -0300

    Commit 15

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -6,5 +6,9 @@ public class NewReleasePrice extends Price {
 
     public double getCharge(int daysRented){
       return daysRented * 3;
-    } 
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+      return (daysRented > 1) ? 2: 1;
+    }
 }
